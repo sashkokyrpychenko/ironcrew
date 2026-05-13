c8f6cfca1795f27a1e751dcab578b6f6a2e90350
OLEKSANDR <sashkokyrpychenko@gmail.com>
2026-05-13 19:38:23 +0300

    fix: supabase env vars

--- src/supabase.js
+++ src/supabase.js
@@ -1,4 +1,6 @@
-import {createClient} from '@supabase/supabase-js';
-const url = 'https://ipeflzuidnbckisddwdz.supabase.co';
-const key = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlwZWZsenVpZG5iY2tpc2Rkd2R6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg1MTk3MjIsImV4cCI6MjA5NDA5NTcyMn0.jsvfZR9IlqagfJN7lqIfad7iF9lgyrY-2_xhFKUXRcE';
+import { createClient } from '@supabase/supabase-js';
+
+const url = import.meta.env.VITE_SUPABASE_URL;
+const key = import.meta.env.VITE_SUPABASE_ANON_KEY;
+
 export const supabase = createClient(url, key);
